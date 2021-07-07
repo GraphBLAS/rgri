d960040d8c86c52dd0c99fc5e1eddbd8c25c74a2
Benjamin Brock <brock@cs.berkeley.edu>
2021-07-06 18:05:57 -0700

    Add some documentation to Matrix

--- include/grb/containers/matrix.hpp
+++ include/grb/containers/matrix.hpp
@@ -16,13 +16,20 @@
 
 namespace grb {
 
+/// A GraphBLAS matrix object.
+/// 1. Template parameter `T` specifies the type of values stored in the matrix.
+/// 2. `I` is an integer type used to record the indices of stored elements.
+/// 3. `Hint` is a hint as to what backend data structure should be used to
+///    store the matrix elements, and can be either `grb::sparse` or `grb::dense`.
 template <typename T,
           typename I = std::size_t,
           typename Hint = grb::sparse,
           typename Allocator = std::allocator<T>>
 class matrix {
 public:
+  /// Type of values stored in matrix.
   using value_type = T;
+  /// Type used to store indices of stored values.
   using index_type = I;
   using allocator_type = Allocator;
   using size_type = std::size_t;
@@ -44,6 +51,7 @@ public:
 
   using tuples_type = std::vector<std::tuple<value_type, index_type, index_type>>;
 
+  /// Default constructor. Creates an empty matrix with zero rows and columns.
   matrix() = default;
   matrix(const matrix&) = default;
   matrix& operator=(const matrix&) = default;
@@ -55,22 +63,28 @@ public:
     backend_matrix_ = backend_type(index_t{*dimensions.begin(), *(dimensions.begin()+1)});
   }
 
+  /// Create an empty matrix of specified dimension.
   matrix(index_t dimensions) : backend_matrix_(dimensions) {}
 
+  /// Create a new matrix market from the MatrixMarket file at path `fname`.
   matrix(std::string fname) : backend_matrix_(read_MatrixMarket<T, I>(fname)) {}
 
+  /// Return the shape, or dimensions, of the matrix.
   index_t shape() const noexcept {
     return {backend_matrix_.m(), backend_matrix_.n()};
   }
 
+  /// Return the number of stored values in the matrix.
   size_type size() const noexcept {
     return backend_matrix_.nnz();
   }
 
+  /// Iterator to the first element stored in the matrix.
   iterator begin() {
     return iterator(backend_matrix_.begin());
   }
 
+  /// Iterator to one past the last stored element in the matrix.
   iterator end() {
     return iterator(backend_matrix_.end());
   }
@@ -85,6 +99,8 @@ public:
 
   // TODO: write const versions of find() and at()
 
+  /// Return an iterator to the element at index `index`, if it exists,
+  /// or `end()`, if the element is not present.
   iterator find(index_t index) {
     if (index[0] >= shape()[0] || index[1] >= shape()[1]) {
       return end();
@@ -99,6 +115,9 @@ public:
     return const_iterator(backend_matrix_.find(index));
   }
 
+  /// Return a reference to the stored element at index `index`.  If the
+  /// element is not present, first stores a default value (`value_type()`)
+  /// at index `index` before returning a reference.
   reference operator[](index_t index) {
     auto iter = find(index);
     if (iter == end()) {
