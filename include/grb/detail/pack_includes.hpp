207176efca0a511105dd313a4c3fef8472eb8374
Benjamin Brock <bbrock4@utk.edu>
2021-07-16 15:58:26 -0400

    Update hints to include grb::compose

---
+++ include/grb/detail/pack_includes.hpp
@@ -0,0 +1,29 @@
+#pragma omp
+
+namespace grb {
+
+template <typename...>
+struct pack_includes_impl_;
+
+template <typename T, typename U, typename... Us>
+struct pack_includes_impl_<T, U, Us...> {
+  static constexpr bool value = std::is_same<T, U>::value || pack_includes_impl_<T, Us...>::value;
+};
+
+template <typename T, typename U>
+struct pack_includes_impl_<T, U> {
+  static constexpr bool value = std::is_same<T, U>::value;
+};
+
+template <typename T>
+struct pack_includes_impl_<T> {
+  static constexpr bool value = false;
+};
+
+// check if Ts includes the type T
+template <typename T, typename... Ts>
+struct pack_includes {
+  static constexpr bool value = pack_includes_impl_<T, Ts...>::value;
+};
+
+} // end grb
--- include/grb/util/matrix_hints.hpp
+++ include/grb/util/matrix_hints.hpp
@@ -2,13 +2,24 @@
 
 #include <grb/containers/backend/csr_matrix.hpp>
 #include <grb/containers/backend/dense_matrix.hpp>
+#include <grb/detail/pack_includes.hpp>
 
 namespace grb {
 
 struct sparse {};
 struct dense {};
+struct row {};
+struct column {};
 
-template <typename T>
+template <typename... Hints>
+struct compose {
+	template <typename T>
+	struct includes {
+	  static constexpr bool value = pack_includes<T, Hints...>::value;
+	};
+};
+
+template <typename T, typename Enabler = void>
 struct pick_backend_type;
 
 template <>
@@ -23,17 +34,40 @@ struct pick_backend_type<dense> {
 	using type = grb::dense_matrix_impl_<Args...>;
 };
 
-template <typename T, typename U>
-struct pick_ewise_hint {
-	using type = grb::dense;
+template <typename... Hints>
+struct pick_backend_type<compose<Hints...>,
+                         std::enable_if_t<compose<Hints...>:: template includes<grb::sparse>::value>
+                         >
+{
+  template <typename... Args>
+  using type = grb::csr_matrix_impl_<Args...>;
 };
 
-// TODO: once hints are expanded, will need
-//       to check whether hint *contains* sparse,
-//       dense.  Also properly handle dense.
-template <>
-struct pick_ewise_hint<grb::sparse, grb::sparse> {
+template <typename... Hints>
+struct pick_backend_type<compose<Hints...>,
+                         std::enable_if_t<
+                           compose<Hints...>:: template includes<grb::dense>::value &&
+                           !compose<Hints...>:: template includes<grb::sparse>::value
+                                         >
+                         >
+{
+  template <typename... Args>
+  using type = grb::csr_matrix_impl_<Args...>;
+};
+
+// Current logic: if one of the matrices is dense,
+// pick dense. Otherwise, sparse.
+template <typename T, typename U, typename Enabler = void>
+struct pick_ewise_hint {
 	using type = grb::sparse;
+};
+
+template <typename T, typename U>
+struct pick_ewise_hint<T, U,
+                       std::enable_if_t<compose<T, U>:: template includes<grb::dense>::value>
+                       >
+{
+	using type = grb::dense;
 };
 
 } // end grb
