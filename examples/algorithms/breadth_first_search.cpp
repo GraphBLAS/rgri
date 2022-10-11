6263a8dc0bd47316279eb5d770303b88c6881413
Benjamin Brock <benjamin.brock@intel.com>
2022-10-11 16:58:11 -0700

    Implement SSSP, update BFS

--- examples/algorithms/breadth_first_search.cpp
+++ examples/algorithms/breadth_first_search.cpp
@@ -17,33 +17,33 @@ int main(int argc, char** argv) {
   // Import graph
   grb::matrix<int> a("../data/chesapeake.mtx");
 
-  // Create vector
-  grb::vector<int> x(a.shape()[1]);
+  // Create vector to represent our frontier
+  grb::vector<bool> x(a.shape()[1]);
 
-  // Pick a random vertex at which to start
+  // Pick a random starting vertex.
   int vertex = pick_random_vertex(a);
+  x[vertex] = true;
 
   std::cout << "Starting at vertex " << vertex << std::endl;
 
-  x[vertex] = 1;
-
   grb::vector<bool> mask(x.shape());
   mask[vertex] = true;
-
-  bool v = mask[vertex];
 
   size_t iteration = 0;
 
   while (x.size() > 0) {
     std::cout << "Iteration " << iteration << ":" << std::endl;
-    grb::print(x, "X");
+    grb::print(x, "Nodes Visited This Iteration");
 
-    grb::print(mask, "Mask");
+    grb::print(mask, "All Nodes Previously Visited");
 
-    auto b = grb::multiply(grb::transpose(a), x, grb::plus{}, grb::times{}, grb::complement_view(mask));
+    auto b = grb::multiply(grb::transpose(a), x,
+                           grb::plus{}, grb::times<int, bool, bool>{},
+                           grb::complement_view(mask));
 
     auto new_mask = grb::ewise_union(b, mask, [](auto&&, auto&&) -> bool { return true; });
     std::swap(new_mask, mask);
+
 
     std::swap(x, b);
     iteration++;
---
+++ examples/algorithms/sssp.cpp
@@ -0,0 +1,65 @@
+#include <grb/grb.hpp>
+#include <iterator>
+#include <iostream>
+
+template <typename M>
+size_t pick_random_vertex(M&& matrix) {
+  size_t index_id = lrand48() % matrix.size();
+  auto iter = matrix.begin();
+  std::advance(iter, index_id);
+  auto&& [idx, _] = *iter;
+  auto&& [i, j] = idx;
+  return j;
+}
+
+int main(int argc, char** argv) {
+  // srand48(0);
+  // Import graph
+  grb::matrix<int> a("../data/chesapeake.mtx");
+
+  // Create vector to hold distances to each vertex
+  grb::vector<int> dist(a.shape()[1]);
+
+  // Pick a random vertex from which to start
+  int vertex = pick_random_vertex(a);
+  dist[vertex] = 0;
+
+  std::cout << "Starting at vertex " << vertex << std::endl;
+
+  size_t iteration = 0;
+
+  bool updated_vertex = true;
+
+  while (true) {
+    std::cout << "Iteration " << iteration << ":" << std::endl;
+    grb::print(dist, "Current distances");
+
+    updated_vertex = false;
+    auto signaling_min = [&](auto&& a, auto&& b) {
+                           auto min = grb::min{}(a, b);
+                           if (min != a) {
+                             printf("Setting update!\n");
+                             updated_vertex = true;
+                           }
+                           return min;
+                         };
+
+    auto update = grb::multiply(grb::transpose(a), dist,
+                                grb::min{}, grb::plus{},
+                                grb::full_vector_mask{});
+
+    grb::print(update, "update");
+
+    auto dist2 = grb::ewise_union(dist, update, signaling_min);
+
+    printf("%s\n", (updated_vertex || dist.size() != dist2.size()) ? "was an update" : "was not an update");
+
+    if (updated_vertex || dist.size() != dist2.size()) {
+      std::swap(dist, dist2);
+    } else {
+      break;
+    }
+  }
+
+  return 0;
+}
