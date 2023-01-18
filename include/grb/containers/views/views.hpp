898a651ea39b1e37f629e60d71b0343ea84e9112
Benjamin Brock <benjamin.brock@intel.com>
2023-01-18 11:54:11 -0800

    Move `mdspan` support into views

--- include/grb/containers/views/views.hpp
+++ include/grb/containers/views/views.hpp
@@ -29,3 +29,7 @@ inline constexpr auto values = std::ranges::views::elements<1>;
 #include <grb/containers/views/complement_view.hpp>
 #include <grb/containers/views/masked_view.hpp>
 #include <grb/containers/views/traditional_formats/csr_matrix_view.hpp>
+
+#if __has_include(<experimental/mdspan>)
+#include <grb/containers/views/third_party/mdspan_view.hpp>
+#endif
