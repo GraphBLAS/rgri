ddcd60d8fc1c486f4a76208f0acb031cd0ee11d6
Benjamin Brock <benjamin.brock@intel.com>
2023-12-14 13:23:21 -0800

    Fix bug in clang

---
+++ include/grb/detail/detect_index.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <concepts>
+
+namespace grb {
+
+template <std::integral T>
+class index;
+
+namespace __detail {
+
+template <typename T>
+struct instantiation_of_index {
+  static constexpr bool value = false;
+};
+
+template <typename T>
+struct instantiation_of_index<index<T>> {
+  static constexpr bool value = true;
+};
+
+template <typename T>
+inline constexpr bool instantiation_of_index_v = instantiation_of_index<T>::value;
+
+} // end __detail
+
+} // end grb
